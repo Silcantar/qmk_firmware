838f93f3e5a6de17a7ae4bcc0924caf5b9b667b8
Silcantar <joshdlucas@gmail.com>
2025-05-19 19:18:41 -0500

    Add keymap for Ploopy Classic.

--- keyboards/ploopyco/trackball/keymaps/Silcantar/keymap.c
+++ keyboards/ploopyco/trackball/keymaps/Silcantar/keymap.c
@@ -1,4 +1,5 @@
-/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+/* Copyright 2025 Joshua Lucas
+ * Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
  * Copyright 2019 Sunjun Kim
  * Copyright 2020 Ploopy Corporation
  *
@@ -17,9 +18,36 @@
  */
 #include QMK_KEYBOARD_H
 
+#include "01_alias.c"
+
+#include "02_macros.c"
+
+#include "03_tapdance.c"
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( /* Base */
-        KC_BTN1, KC_BTN3, KC_BTN2,
-          KC_BTN4, KC_BTN5
-    ),
+    [_DEFAULT] = LAYOUT(
+/*		
+ * Layer 0: Default Layer
+ *		,-------------------,-------------------,-------------------,-------------------,-------------------.
+ *		|					|					|					|					|					|
+ *		|		Mouse		|	 MO Layer 1		|   MO Drag Scroll  |		Mouse		|		Mouse		|
+ *		|	   Button 1		|	Double Click	|   TG Drag Scroll  |	   Button 2		|	   Button 3		|
+ *		|					|					|				    |					|					|
+ *		`-------------------'-------------------'-------------------'-------------------'-------------------'
+ */
+		  KC_BTN1			, CONFIG_DBLCLK		, TD(TD_DRAG_SCROLL), KC_BTN2			, KC_BTN3
+	),
+
+	[_CONFIG] = LAYOUT(
+/*		
+ * Layer 1: Configuration
+ *		,-------------------,-------------------,-------------------,-------------------,-------------------.
+ *		|					|					|					|					|					|
+ *		|		Trans		|		Trans		|		Print		|	  Cycle	Drag	|	  Bootloader	|
+ *		|					|					|		 DPI		|		Scroll		|					|
+ *		|					|					|					|					|					|
+ *		`-------------------'-------------------'-------------------'-------------------'-------------------'
+ */
+		  KC_TRNS			, KC_TRNS			, PRINT_DPI			, SCROLL_CONFIG		, QK_BOOT
+	)
 };
